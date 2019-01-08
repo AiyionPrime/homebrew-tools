d099e6e0b595f3b2888745d8602c8cea8cdb97b6
aiyion.prime <git@aiyionpri.me>
2019-01-08 20:02:25 +0100

    v1.1

--- Formula/heimdallr.rb
+++ Formula/heimdallr.rb
@@ -1,8 +1,8 @@
 class Heimdallr < Formula
   desc "Finding public ssh keys with ease"
   homepage "https://github.com/AiyionPrime/heimdallr"
-  url "https://github.com/AiyionPrime/heimdallr/archive/v1.0.3.tar.gz"
-  sha256 "b13798e4e7cae06b93829532d94337487416ec4b7dbf7d79a6bdb1077b8cedbf"
+  url "https://github.com/AiyionPrime/heimdallr/archive/v1.1.tar.gz"
+  sha256 "7d9cfb50c84dbc5b4616943ddb3e430a8ad8a1fb5cc109fa37dee556eda0ef37"
   depends_on "json-c"
   depends_on "libssh"
   depends_on "openssl"
